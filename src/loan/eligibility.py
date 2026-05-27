fedcc511dcc40aac472d8a3a34ca3659c7f35333
Jose Chong <127264125+Jlchong3@users.noreply.github.com>
2026-05-27 15:57:52 -0500

    pylint(W0612): Remove unused variable tmp

--- src/loan/eligibility.py
+++ src/loan/eligibility.py
@@ -50,7 +50,6 @@ def evaluate(profile: ApplicantProfile):
     # Temporary buffers for intermediate calculation. Will be cleaned up later.
     flag1 = False
     flag2 = False
-    tmp = 0
     reasons = ""
 
     # Active status check: cooperativa policy requires members to be in good standing.
